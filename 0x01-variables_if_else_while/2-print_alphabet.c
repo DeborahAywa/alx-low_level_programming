935cc0fbf8950b7eda02fe8f1163b0728cef8767
DeborahAywa <debraokinda@gmail.com>
2023-03-19 08:57:08 -0700

    print the alhabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main- main function
+ *
+ * Return:returns the program 0
+ */
+
+int main(void)
+{
+	char x;
+	for (x = 'a'; x <= 'z'; x++)
+		putchar (x);
+	putchar ('\n');
+	return (0);
+}
