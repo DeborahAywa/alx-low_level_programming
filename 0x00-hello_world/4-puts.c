2ced4309301d8d6147a741b430105b113fa6af55
DeborahAywa <debraokinda@gmail.com>
2023-03-20 06:10:26 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,11 @@
 /**
  * main - function that prints text
  *
- * Return:program ends with the value 0
+ * Return:program ends with  a 0
  */
+
 int main(void)
-{																			puts("\"Programming is like building a multilingual puzzle");											return(0);
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
