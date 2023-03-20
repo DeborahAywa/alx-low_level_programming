113827668dcd9c8eb7671f75d54d9c07c8b9f2d2
DeborahAywa <debraokinda@gmail.com>
2023-03-20 05:53:56 -0700

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,10 @@
  *
  * Return:program returns 0
  */
-int main(void)																		{																			printf("with proper grammar, but the outcome is a piece of art,\n");
-		return(0);
-	}
+
+int main(void)
+{
+	printf("with proper grammar, butthe outcome is a piece of art,");
+	printf("\n");
+	return (0);
+}
