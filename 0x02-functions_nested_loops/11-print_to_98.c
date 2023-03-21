3ff36ae454b7343d0599f012826652db056ab240
DeborahAywa <debraokinda@gmail.com>
2023-03-21 13:28:32 -0700

    natural numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_to_98-function prinyting natural numbers to 98
+ *
+ * @n:parameter to be checked
+ *
+ */
+
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				_putchar(i + '0');
+			else if (i == 98)
+				_putchar(i'\n' + '0');
+		}
+	}
+	else if (n > 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				_putchar(j + '0');
+			else if (j == 98)
+				_putchar(j'\n' + '0');
+		}
+	}
+}
