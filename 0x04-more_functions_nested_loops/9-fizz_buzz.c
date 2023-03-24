0657d7a1aa8cc3289602dfd5a48163665f8c795c
DeborahAywa <debraokinda@gmail.com>
2023-03-24 04:49:29 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,23 +17,19 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz");
-			printf(" ");
+			printf("Fizz ");
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz");
-			printf(" ");
+			printf("Buzz ");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
-			printf(" ");
+			printf("FizzBuzz ");
 		}
 		else
 		{
-			printf("%d", i);
-			printf(" ");
+			printf("%d ", i);
 		}
 	}
 	printf("\n");
