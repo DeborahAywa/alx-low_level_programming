60c1f90759a541698b04b3be7cd1a5e274686f51
DeborahAywa <debraokinda@gmail.com>
2023-03-27 11:46:39 -0700

    Print sting in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev-function prints string in reverse
+ *
+ * @s:string to be printed
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	s--;
+	for (i = length; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
